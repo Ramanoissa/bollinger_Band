ca6787b908d26ec0b34163d5bb07a5d84a7a579c
Ramanoissa <127023946+Ramanoissa@users.noreply.github.com>
2023-07-04 20:01:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import tele as tb
 from binanceapi import client
 import numpy as np
 import pandas as pd
+
+
+for symbol in symbols:
+    # Get data from API
+    klines = []
+    get_data
+    bollinger
